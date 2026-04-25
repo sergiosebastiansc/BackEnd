9fa00998b8da07d78ccd6e74269e05f68accb511
Sergio Salinas Campos <31582822+sergiosebastiansc@users.noreply.github.com>
2026-04-25 19:00:24 -0400

    Update espacios.controllers.js

--- src/controllers/espacios.controllers.js
+++ src/controllers/espacios.controllers.js
@@ -1,27 +1,42 @@
-const { getAllEspacios } = require("../models/espacios.models")
-
+const { getAllEspacios } = require("../models/espacios.models");
 
 const obtenerEspacios = async (req, res) => {
-    try {
-        // pide los datos
+
+    try {            
         const espacios = await getAllEspacios(); 
-        // respuesta al cliente
         res.status(200).json(espacios) 
-    } catch (error) {
-        console.log("error",error)
+        } catch (error) {
+         console.log("error",error)
         res.status(500).json({
             msg: "Error de servidor."
+            
+       
         })
     }
-   
-}
+};
 
-const crearEspacios = async (req, res) => {
-    res.json({
-        msg: "Crear espacios"
-    })
-}
+const crearEspacios = async (req, res, next) => {
 
+    const { nombre, capacidad, precio, ubicacion, disponibilidad } = req.body;
+
+    try {        
+        const nuevoId = Date.now();
+         const nuevoEspacio = {
+            id: nuevoId,
+            nombre,
+            capacidad,
+            precio,
+            ubicacion,
+            disponibilidad
+         };
+    
+         const EspacioCreado = await createEspacio(nuevoEspacio);
+    
+         res.status(201).json(espacioCreado);
+    } catch (error) {
+        next(error);       
+    }
+};
 
 module.exports = {
     obtenerEspacios,
