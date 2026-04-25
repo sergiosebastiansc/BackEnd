6bee5170ca52c440a5976a89866965e0cb08753a
Sergio Salinas Campos <31582822+sergiosebastiansc@users.noreply.github.com>
2026-04-25 18:53:20 -0400

    Update reservas.controllers.js

--- src/controllers/reservas.controllers.js
+++ src/controllers/reservas.controllers.js
@@ -1,11 +1,9 @@
-const { getAllReservas, createReserva } = require("../models/reservas.models");
+const { getAllReservas, createReserva, updateReserva, deleteReserva } = require("../models/reservas.models");
 
 
 const obtenerReservas = async (req, res) => {
     try {
-        // pide los datos 
-        const reservas = await getAllReservas();
-        // respuesta al cliente 
+        const reservas = await getAllReservas(); 
         res.status(200).json(reservas) 
     } catch (error) {
         console.log("error",error)
@@ -15,7 +13,7 @@ const obtenerReservas = async (req, res) => {
     }
 };
 
-const crearReserva = async (req, res) => {
+const crearReserva = async (req, res, next) => {
 
     const { espacioId, fecha, horaInicio, horaFin } = req.body;
 
@@ -33,16 +31,44 @@ const crearReserva = async (req, res) => {
     
          res.status(201).json(reservaCreada);
     } catch (error) {
-         console.log("error",error)
-         res.status(500).json({
-            msg: "Error de servidor."
-        })
+        next(error);       
     }
+};
 
-}
+const actualizarReserva = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const actualizada = await ReservaModel.updateReserva(id, req.body);
 
+        if (!actualizada) {
+            return res.status(404).json({ message: "Reserva no encontrada" });
+        }
+
+        res.status(200).json({ message: "Reserva Actualizada", data: actualizada });
+    } catch (error) {
+        res.status(500).json({ error: "Error interno del servidor" });
+    }
+};
+
+const borrarReserva = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const eliminado = await ReservaModel.deleteReserva(id);
+
+        if (!eliminado) {
+            return res.status(404).json({ message: "No se pudo eliminar: ID inexistente" });
+        }
+
+        res.status(200).json({ message: "Reserva eliminada correctamente" });
+    } catch (error) {
+        res.status(500).json({ error: "Error al eliminar" });
+    }
+};
 
 module.exports = {
     obtenerReservas,
-    crearReserva
+    crearReserva,
+    actualizarReserva,
+    borrarReserva
+
 }
