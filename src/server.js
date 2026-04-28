b622c846ef3d5dcd187c25b183eb5b1de60b111a
Sergio Salinas Campos <31582822+sergiosebastiansc@users.noreply.github.com>
2026-04-28 10:50:49 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@
 const app = require("./app");
 
 const PORT = process.env.PORT || 3000;
-
+const { connect } = require("./database/mongoose");
 
 app.listen(PORT, ()=> {
     console.log(`Servidor corriendo en http://localhost:${PORT}`)
